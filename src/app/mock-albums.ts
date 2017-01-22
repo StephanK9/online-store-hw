833c22c3e966d777c21b4c11d8444adbdec833e9
StephanK9 <stephane.khoury@gmail.com>
2017-01-22 12:02:13 -0800

    add dynamic routing, dependecy injection, services, online-store app is fully operational as of Tuesday morning specs

---
+++ src/app/mock-albums.ts
@@ -0,0 +1,10 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] = [
+ new Album('Pulse', 'Pink Floyd', 'A live  album by the English progressive rock band originally released in 1995.', 1),
+ new Album('Funhouse', 'The Stooges', 'The second  album from the American rock band.', 2),
+ new Album('Twilight of the Thunder God', 'Amon Amarth', 'Seventh album by the Swedish band, released in 2008.', 3),
+ new Album('Dilate', 'Ani DiFranco', 'Her highest-selling and most acclaimed album, released in 1996.', 4),
+ new Album('Chopin - Complete Nocturnes', 'Brigitte Engerer', 'Released in 2010.', 5),
+ new Album('Axis Bold As Love', 'The Jimi Hendrix Experience', 'Second studio album by the English-American band.', 6)
+];
